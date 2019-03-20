76c2d34865354738700f505ecb50a00cc2399ecf
HecnfvG <rust.gum@gmail.com>
2019-03-20 22:01:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+
+const config = require('./config');
+
+
+// add ruoter
+const routes = require('./routes');
+
+const app = express();
+
+// database
+mongoose.Promise = global.Promise;
+mongoose.set('debug', config.IS_PRODUCTION);
+mongoose.connection
+    .on('error', error => console.log(error))
+    .on('close', () => console.log('Database connection closed.'))
+    .once('open', () => {
+        const info = mongoose.connections[0];
+        console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+    });
+mongoose.connect(config.MONGO_URL, { useNewUrlParser: true });
+
+// sessions
+app.use(
+    session({
+        secret: config.SESSION_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        store: new MongoStore({
+            mongooseConnection: mongoose.connection
+        }),
+        //expires: new Date(Date.now() + 60 * 60 * 24 * 30)
+        expires: new Date(Date.now() + 10)
+    })
+);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// use router
+app.use('/Admin', routes.Admin);
+app.use('/Service', routes.Service);
+app.use('/Client', routes.Client);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
